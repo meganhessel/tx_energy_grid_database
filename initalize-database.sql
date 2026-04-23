1d851ca2ab12b19222402936a057e634a10728fc
Megan Hessel <meganhessel@ucsb.edu>
2026-04-22 21:56:40 -0700

    sql addition

---
+++ initalize-database.sql
@@ -0,0 +1,25 @@
+
+
+-- Create a Energy table 
+CREATE TABLE 
+
+
+-- Create a new, empty table 
+CREATE TABLE Snow_cover (
+    Site VARCHAR NOT NULL,
+    Year INTEGER NOT NULL CHECK (Year BETWEEN 1990 AND 2018),
+    Date DATE NOT NULL,
+    Plot VARCHAR NOT NULL,
+    Location VARCHAR NOT NULL,
+    Snow_cover REAL CHECK (Snow_cover BETWEEN 0 AND 130),
+    Water_cover REAL CHECK (Water_cover BETWEEN 0 AND 130),
+    Land_cover REAL CHECK (Land_cover BETWEEN 0 AND 130),
+    Total_cover REAL CHECK (Total_cover BETWEEN 0 AND 130),
+    Observer VARCHAR,
+    Notes VARCHAR,
+    PRIMARY KEY (Site, Plot, Location, Date),
+    FOREIGN KEY (Site) REFERENCES Site (Code)
+);
+
+-- Fill the empty table with the csv 
+COPY Snow_cover FROM "../ASDN_csv/snow_survey_fixed.csv" (header TRUE, nullstr "NA");
